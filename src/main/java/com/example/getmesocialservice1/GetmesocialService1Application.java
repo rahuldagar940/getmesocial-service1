48b6ccca3842dc349c075fa6afe6606fff9a9a25
rahuldagar <rahuldagar940@gmail.com>
2022-04-05 05:34:48 -0400

    first commit

---
+++ src/main/java/com/example/getmesocialservice1/GetmesocialService1Application.java
@@ -0,0 +1,13 @@
+package com.example.getmesocialservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetmesocialService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetmesocialService1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/getmesocialservice1/GetmesocialService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getmesocialservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetmesocialService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
